c8043b232b201703563349c5c2d14ab317ca5bf7
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-12-04 09:36:54 +0100

    demo

--- src/main/java/io/quarkiverse/langchain4j/chat/demo/DocumentIngestor.java
+++ src/main/java/io/quarkiverse/langchain4j/chat/demo/DocumentIngestor.java
@@ -1,38 +1,44 @@
 package io.quarkiverse.langchain4j.chat.demo;
 
+import static io.qdrant.client.grpc.Points.PointsUpdateOperation.OperationCase.UPSERT;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.apache.camel.component.qdrant.Qdrant;
+import static org.apache.camel.component.qdrant.Qdrant.Headers.ACTION;
 import org.apache.camel.component.qdrant.QdrantAction;
-import org.apache.camel.model.tokenizer.LangChain4jTokenizerDefinition;
 import org.apache.camel.spi.DataType;
 
+import static org.apache.camel.model.tokenizer.LangChain4jTokenizerDefinition.TokenizerType.OPEN_AI;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @ApplicationScoped
-public class DocumentIngestor extends RouteBuilder {
+public class DocumentIngestor extends EndpointRouteBuilder {
+   private static final DataType DATA_TYPE_QDRANT_EMBEDDINGS = new DataType("qdrant:embeddings");
+
+    @ConfigProperty(name = "aws-s3.bucket-name")
+    String bucketName;
 
 
     @Override
     public void configure() throws Exception {
-        from("aws2-s3:{{s3.bucket.name}}")
-            .log("Incoming message from S3 bucket ${body}")
-            .convertBodyTo(String.class)  // Convert the file content to a String
-            .tokenize(tokenizer()
+        from(aws2S3(bucketName))
+                .log("Incoming file")
+                .convertBodyTo(String.class)  // Convert the file content to a String
+                // split the documents
+                .tokenize(tokenizer()
                         .byParagraph()
                         .maxTokens(1024)
                         .maxOverlap(10)
-                        .using(LangChain4jTokenizerDefinition.TokenizerType.OPEN_AI)
-                        .end())
+                        .using(OPEN_AI)
+                        .end()
+                )
                 .split().body()
-                .to("direct:embed");
 
-        from("direct:embed")
-                .log("body :: ${body}")
-            .to("langchain4j-embeddings:test")  // Use Langchain4j to generate embeddings
-            .transform(new DataType("qdrant:embeddings"))   // Transform the data type to a format suitable for Qdrant using a predefined transformer
-            .setHeader(Qdrant.Headers.ACTION, constant(QdrantAction.UPSERT))  // Set the header to perform an UPSERT operation in Qdrant
-            .to("qdrant:my-collection");  // Send the data to Qdrant
-
-
+                .to(langchain4jEmbeddings("test"))
+                .transform(DATA_TYPE_QDRANT_EMBEDDINGS)
+                .setHeader(ACTION, constant(UPSERT))  // Set the header to perform an UPSERT operation in Qdrant
+                .to(qdrant("my-collection"));  // Send the data to Qdrant
     }
 
 }
